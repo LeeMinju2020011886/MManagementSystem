6ce39a4c5a60589ab23987faaff2894b23da6f73
LeeMinju <xosxos2@naver.com>
2021-03-16 15:47:59 +0900

    Write MoneyManagement(Pocket) code

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -2,11 +2,11 @@
 public class MenuManagement {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
-		System.out.println("5. Exit");
+		System.out.println("1. Deposit");		//예금
+		System.out.println("2. Withdrawal");	//출금
+		System.out.println("3. View balance");	//잔고 확인
+		System.out.println("4. Edit system");	//수정(이름, 메모 등)
+		System.out.println("5. Exit");			
 
 	}
 
