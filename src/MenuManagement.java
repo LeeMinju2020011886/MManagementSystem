30d35bf95f5798b23ca1b0b1a285af0bbafe6a55
LeeMinju <xosxos2@naver.com>
2021-03-29 13:26:41 +0900

    edit menu by methods

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -6,7 +6,7 @@ public class MenuManagement {
 		
 		int num = 0;
 		Scanner input = new Scanner(System.in);
-		
+		// 처음에 현재 잔고를 적는 것을 시작으로..
 		while (num != 6) {
 			System.out.println("1. Deposit");		//예금
 			System.out.println("2. Withdrawal");	//출금
@@ -17,20 +17,63 @@ public class MenuManagement {
 			System.out.print("Select one number between 1 - 5:");
 			num = input.nextInt();
 			System.out.println();
-			
-			switch(num) {
+			if (num == 1) {
+				addDeposit();
+			}
+			if (num == 2) {
+				addWithdrawal();
+			}
+			if (num == 3) {
+				ViewBalance();
+			}
+			if (num == 4) {
+				EditSystem();
+			}
+		}
+	}	
+	public static void addDeposit() {
+		System.out.println("<Deposit>");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Amount : ");
+		int DepositAmount = input.nextInt();
+		System.out.print("Category : ");
+		String DepositName = input.next();		//메뉴랑 같이 출력 안되려면?
+		System.out.print("memo: ");
+		String DepositMemo = input.next();
+	}
+	
+	public static void addWithdrawal() {
+		System.out.println("<Withdrawal>");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Amount : ");
+		int WithdrawalAmount = input.nextInt();
+		System.out.print("Category : ");
+		String WithdrawalName = input.next();		//메뉴랑 같이 출력 안되려면?
+		System.out.print("memo: ");
+		String WithdrawalMemo = input.next();
+	}
+	
+	public static void ViewBalance() {
+		System.out.println("<View Balance>");
+	}
+	
+	public static void EditSystem() {
+		System.out.println("<Edit System>");
+	}
+	
+			/*switch(num) {
 				case 1:
 					int num2 = 0;
-					while (num2 != 2) {
+					@@@@@@@@@@@@@@@@@@@while (num2 != 2) {
 						System.out.println("<Amount>");
 						System.out.print("Name : ");
 						String DepositName = input.next();
 						System.out.print("Amount : ");
 						int DepositAmount = input.nextInt();
 						System.out.println("Continue?");
-						System.out.println("1. Yes");
-						System.out.println("2. No");
-						num2 = input.nextInt();			//int num2 = 는 왜 안됐던거지??
+					@@@@@@@@@@@@@@@@@@@System.out.println("1. Yes");
+					@@@@@@@@@@@@@@@@@@@System.out.println("2. No");
+					@@@@@@@@@@@@@@@@@@@num2 = input.nextInt();			//@는 나중에 추가할 거!! 표시
 					}
 					break;
 				case 2:
@@ -43,7 +86,6 @@ public class MenuManagement {
 				case 3:
 				case 4:
 				case 5:
-			}
-		}
-	}
+			}*/
+
 }
