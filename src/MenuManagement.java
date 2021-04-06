f7c5988a528715f14d2ccb87f276a4d776c687d7
LeeMinju <xosxos2@naver.com>
2021-04-06 16:34:08 +0900

    edit menu management (have error)

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -1,65 +1,35 @@
 import java.util.Scanner;
 
 public class MenuManagement {
-
 	public static void main(String[] args) {
-		
 		int num = 0;
 		Scanner input = new Scanner(System.in);
+		MoneyManage moneyManage = new MoneyManage(input);
 		// 처음에 현재 잔고를 적는 것을 시작으로..
-		while (num != 6) {
-			System.out.println("1. Deposit");		//예금
-			System.out.println("2. Withdrawal");	//출금
-			System.out.println("3. View Balance");	//잔고 확인
-			System.out.println("4. Edit System");	//수정(이름, 메모 등)
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");			
+		while (num != 5) {
+			System.out.println("1. Deposit");		
+			System.out.println("2. Withdrawal");	
+			System.out.println("3. View Balance");	
+			System.out.println("4. Edit System");	
+			System.out.println("5. Exit");			
 			System.out.print("Select one number between 1 - 5:");
 			num = input.nextInt();
 			System.out.println();
 			if (num == 1) {
-				addDeposit();
+				moneyManage.addDeposit();
 			}
 			if (num == 2) {
-				addWithdrawal();
+				moneyManage.addWithdrawal();
 			}
 			if (num == 3) {
-				ViewBalance();
+				moneyManage.ViewBalance();
 			}
 			if (num == 4) {
-				EditSystem();
+				moneyManage.EditSystem();
 			}
 		}
 	}	
-	public static void addDeposit() {
-		System.out.println("<Deposit>");
-		Scanner input = new Scanner(System.in);
-		System.out.print("Amount : ");
-		int DepositAmount = input.nextInt();
-		System.out.print("Category : ");
-		String DepositName = input.next();		//메뉴랑 같이 출력 안되려면?
-		System.out.print("memo: ");
-		String DepositMemo = input.next();
-	}
 	
-	public static void addWithdrawal() {
-		System.out.println("<Withdrawal>");
-		Scanner input = new Scanner(System.in);
-		System.out.print("Amount : ");
-		int WithdrawalAmount = input.nextInt();
-		System.out.print("Category : ");
-		String WithdrawalName = input.next();		//메뉴랑 같이 출력 안되려면?
-		System.out.print("memo: ");
-		String WithdrawalMemo = input.next();
-	}
-	
-	public static void ViewBalance() {
-		System.out.println("<View Balance>");
-	}
-	
-	public static void EditSystem() {
-		System.out.println("<Edit System>");
-	}
 	
 			/*switch(num) {
 				case 1:
