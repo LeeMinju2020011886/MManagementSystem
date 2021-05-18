17bc443d4cc27e2bba173b02f838efa6db96f635
LeeMinju <xosxos2@naver.com>
2021-05-18 17:05:35 +0900

    project38
    abstract

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuManagement {
@@ -5,37 +6,55 @@ public class MenuManagement {
 		Scanner input = new Scanner(System.in);
 		MoneyManage moneyManage = new MoneyManage(input);
 		
+		selectMenu(input, moneyManage);
+	}
+	
+	public static void selectMenu(Scanner input, MoneyManage moneyManage) {
 		int num = -1;
 		while (num != 6) {
-			System.out.println("*** Money Management System Menu ***");
-			System.out.println("1. Deposit");		
-			System.out.println("2. Withdrawal");	
-			System.out.println("3. View deals");	
-			System.out.println("4. Edit System");
-			System.out.println("5. delete deal");
-			System.out.println("6. Exit");			
-			System.out.print("Select one number between 1 - 6:");
-			num = input.nextInt();
-			System.out.println();
-			if (num == 1) {
-				moneyManage.addDeposit();
+			
+			try {
+				showMenu();			
+				num = input.nextInt();
+				switch(num) {
+				case 1:
+					moneyManage.addDeposit();
+					break;
+				case 2:
+					moneyManage.addWithdrawal();
+					break;
+				case 3:
+					moneyManage.viewdeals();
+					break;
+				case 4:
+					moneyManage.editSystem();
+					break;
+				case 5:
+					moneyManage.deletedeal();
+					break;
+				default:
+					continue;
+				}
 			}
-			else if (num == 2) {
-				moneyManage.addWithdrawal();
-			}
-			else if (num == 3) {
-				moneyManage.viewdeals();
-			}
-			else if (num == 4) {
-				moneyManage.editSystem();
-			}
-			else if (num == 5) {
-				moneyManage.deletedeal();
-			}
-			else {
-				continue;
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 6");
+				if (input.hasNext()) {
+					input.next();
+				}
+				num = -1;
 			}
 		}
 	}
-
+	
+	public static void showMenu() {
+		System.out.println("*** Money Management System Menu ***");
+		System.out.println("1. Deposit");		
+		System.out.println("2. Withdrawal");	
+		System.out.println("3. View deals");	
+		System.out.println("4. Edit System");
+		System.out.println("5. delete deal");
+		System.out.println("6. Exit");			
+		System.out.print("Select one number between 1 - 6:");
+	
+	}
 }
