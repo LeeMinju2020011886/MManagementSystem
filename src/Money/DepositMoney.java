88bb01470ee301eb0e47610d4cbb35a6182e0d9f
LeeMinju <xosxos2@naver.com>
2021-06-15 18:23:13 +0900

    abstract (5월 11일 제출)
    DepositMoney
    
    제 실수로 5월 11일 제출한 프로젝트를 지금 푸쉬하였습니다.

--- src/Money/DepositMoney.java
+++ src/Money/DepositMoney.java
@@ -1,15 +1,8 @@
 package Money;
 
-import java.io.Serializable;
 import java.util.Scanner;
 
-import exception.AmountFormatException;
-
-public abstract class DepositMoney implements Moneyinput, Serializable {			//abstract >> Deposit이라는 객체를 생성하지 않는다.
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4517492711906983620L;
+public abstract class DepositMoney {			//abstract >> Deposit이라는 객체를 생성하지 않는다.
 	protected Paykind kind = Paykind.Cash;  
 	protected String Category;
 	protected int UniNum;
@@ -78,60 +71,14 @@ public abstract class DepositMoney implements Moneyinput, Serializable {			//abs
 		return Memo;
 	}
 
-	public void setMemo(String memo) throws AmountFormatException {
-		if (!memo.contains("#") && !memo.equals("")) {
-			throw new AmountFormatException();
-		}
+	public void setMemo(String memo) {
 		Memo = memo;
 	}
 
 
 	public abstract void printInfo(); 
 	
-	public void setUniNum(Scanner input) {
-		System.out.print("Unique Number: ");
-		int uniNum = input.nextInt();
-		this.setUniNum(uniNum);
-	}
-	
-	public void setAmount(Scanner input) {
-		System.out.print("Amount: ");
-		int amount = input.nextInt();
-		this.setAmount(amount);
-	}
-	
-	public void setCategory(Scanner input) {
-		System.out.print("Category: ");
-		String category= input.next();
-		this.setCategory(category);
-	}
-	
-	public void setMemo(Scanner input) { 
-		String memo = "";
-		while (!memo.contains("#")) {
-			System.out.print("Memo: ");
-			memo = input.next();
-			try {
-				this.setMemo(memo);
-			} catch (AmountFormatException e) {
-				System.out.println("Incorrect Memo Format. Put the memo that contains # ");
-				
-			}
-		}
-	}
-	public String getKindStirng() {
-		String skind = "none";
-		switch(this.kind) {
-		case Cash:
-			skind = "Cash";
-			break;
-		case Card:
-			skind = "Card";
-			break;
-		default:
-		}
-		return skind;
-	}
+
+
 
 }
-
