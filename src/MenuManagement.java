4882c30ad80d6193cd4f5436c4bc51966185bf81
LeeMinju <xosxos2@naver.com>
2021-06-15 18:14:48 +0900

    abstract (5월 11일 제출)
    MenuManagement
    
    제 실수로 5월 11일에 제출한 프로젝트를 지금 푸쉬하였습니다.

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -1,130 +1,41 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.InputMismatchException;
 import java.util.Scanner;
-import log.EventLogger;
 
 public class MenuManagement {
-	private static final Object MoneyManage = null;
-	static EventLogger logger = new EventLogger("log.txt");
-	
 	public static void main(String[] args) {
-		
 		Scanner input = new Scanner(System.in);
-		MoneyManage moneyManage = getObject("moneymanager.ser");
-		if (MoneyManage == null) {
-			moneyManage = new MoneyManage(input);
-		}
+		MoneyManage moneyManage = new MoneyManage(input);
 		
-		selectMenu(input, moneyManage);
-		putObject(moneyManage, "moneymanager.ser");
-	}
-	
-	public static void selectMenu(Scanner input, MoneyManage moneyManage) {
 		int num = -1;
 		while (num != 6) {
-			
-			try {
-				showMenu();			
-				num = input.nextInt();
-				switch(num) {
-				case 1:
-					moneyManage.addDeposit();
-					logger.log("add a deposit");
-					break;
-				case 2:
-					moneyManage.addWithdrawal();
-					logger.log("add a withdrawal");
-					break;
-				case 3:
-					moneyManage.viewdeals();
-					logger.log("view deals");
-					break;
-				case 4:
-					moneyManage.editSystem();
-					logger.log("edit deals");
-					break;
-				case 5:
-					moneyManage.deletedeal();
-					logger.log("delete a deal");
-					break;
-				default:
-					continue;
-				}
+			System.out.println("*** Money Management System Menu ***");
+			System.out.println("1. Deposit");		
+			System.out.println("2. Withdrawal");	
+			System.out.println("3. View deals");	
+			System.out.println("4. Edit System");
+			System.out.println("5. delete deal");
+			System.out.println("6. Exit");			
+			System.out.print("Select one number between 1 - 6:");
+			num = input.nextInt();
+			System.out.println();
+			if (num == 1) {
+				moneyManage.addDeposit();
 			}
-			catch(InputMismatchException e) {
-				System.out.println("Please put an integer between 1 and 6");
-				if (input.hasNext()) {
-					input.next();
-				}
-				num = -1;
+			else if (num == 2) {
+				moneyManage.addWithdrawal();
+			}
+			else if (num == 3) {
+				moneyManage.viewdeals();
+			}
+			else if (num == 4) {
+				moneyManage.editSystem();
+			}
+			else if (num == 5) {
+				moneyManage.deletedeal();
+			}
+			else {
+				continue;
 			}
 		}
 	}
-	
-	public static void showMenu() {
-		System.out.println("*** Money Management System Menu ***");
-		System.out.println("1. Deposit");		
-		System.out.println("2. Withdrawal");	
-		System.out.println("3. View deals");	
-		System.out.println("4. Edit System");
-		System.out.println("5. delete deal");
-		System.out.println("6. Exit");			
-		System.out.print("Select one number between 1 - 6:");
-	
-	}
-	
-	public static MoneyManage getObject(String filename) {
-		MoneyManage moneyManage = null;
-		
-		
-		
-		try {
-			FileInputStream file = new FileInputStream(filename);
-			ObjectInputStream in = new ObjectInputStream(file);
-			
-			moneyManage = (MoneyManage) in.readObject();
-			
-			in.close();
-			file.close();
-			 
-		} catch (FileNotFoundException e) {
-			return moneyManage;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-		return moneyManage;
-		
-	}
-	
-	public static void putObject(MoneyManage moneyManage, String filename) {
-		try {
-			FileOutputStream file = new FileOutputStream(filename);
-			ObjectOutputStream out = new ObjectOutputStream(file);
-			
-			out.writeObject(MoneyManage);
-			
-			out.close();
-			file.close();
-			
-			
-			 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
 }
